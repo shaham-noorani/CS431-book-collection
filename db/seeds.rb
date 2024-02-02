55d7c0139b8890634fd5929429b5a4981bba74da
Shaham Noorani <shahamnoorani@Shahams-MacBook-Air.local>
2024-02-02 11:47:37 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create([
+  { title: "The Great Gatsby",},
+  { title: "To Kill a Mockingbird",},
+  { title: "1984",},
+  { title: "Pride and Prejudice",},
+])
